a02cab2a52219ee7b02395881ee64d85adf23824
zhaoyuan <zhaoyuan202008@e-chinalife.com>
2021-01-08 14:50:14 +0800

    01.08

--- app/src/main/java/com/example/lint_test/MainActivity.java
+++ app/src/main/java/com/example/lint_test/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.lint_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtn = findViewById(R.id.btn);
     }
 }
